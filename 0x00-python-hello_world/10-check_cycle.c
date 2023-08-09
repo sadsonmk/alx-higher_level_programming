7489a9b0aede49a1cb3361ccd64460f247fde49a
Sadson Mk <sadsonmkumira@yahoo.com>
2023-08-09 08:18:36 -0700

    Adding files

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks whether a linked-list has a cycle or not
+ * @list: is the linked list to be checked
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *behind = list;
+	listint_t *ahead = list;
+
+	while (ahead && ahead->next)
+	{
+		behind = behind->next;
+		ahead = ahead->next->next;
+
+		if (behind == ahead)
+			return (1);
+	}
+
+	return (0);
+}
