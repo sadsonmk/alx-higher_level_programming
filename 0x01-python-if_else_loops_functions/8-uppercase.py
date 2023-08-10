87ba92bcdf56c354bb9169a5136c25b532e178bd
Sadson Mk <sadsonmkumira@yahoo.com>
2023-08-10 09:07:20 -0700

    Adding files

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for x in str:
+        if ord(x) >= 97 and ord(x) <= 123:
+            x = chr(ord(x) - 32)
+        print("{}".format(x), end='')
+    print()
+
+    
