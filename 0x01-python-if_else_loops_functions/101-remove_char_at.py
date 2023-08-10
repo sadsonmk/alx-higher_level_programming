3bb0dadfd181580bcffe9ad5a79d8076204a9d09
Sadson Mk <sadsonmkumira@yahoo.com>
2023-08-10 11:23:54 -0700

    Adding files

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def remove_char_at(str, n):
+    output = ''
+    length = len(str)
+    if n > length -1 or n < 0:
+        return str
+    remove = str[n]
+    for x in str:
+        if x == remove:
+            continue
+        output += x
+    return output
