fa78c81ac46c75b34a48b1a1fa6e56c3ccd1d04d
Sadson Mk <sadsonmkumira@yahoo.com>
2023-08-09 23:45:05 -0700

    Adding files

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 
-for num in range(0, 100):
-    if num < 10:
-        print("0{}".format(num), end=', ')
-    elif num >= 10 and num < 99:
-        print(num, end=', ')
-    else:
-        print(num, end='\n')
+for num in range(0,99):
+    print("{:02d}".format(num), end=', ')
+print(99)
