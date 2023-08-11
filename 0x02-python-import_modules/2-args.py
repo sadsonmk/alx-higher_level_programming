245596179e455ec49518caa85fe39e8cd5474b47
Sadson Mk <sadsonmkumira@yahoo.com>
2023-08-11 01:44:24 -0700

    Adding files

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+from sys import argv
+
+if __name__ == "__main__":
+    length = len(argv)
+
+    if length == 1:
+        print("{} arguments.".format(length - 1))
+    elif length == 2:
+        print("{} argument:".format(length - 1))
+        num = 1
+        for arg in argv:
+            if arg == argv[0]:
+                continue
+            print("{}: {}".format((num), arg))
+            num = num + 1
+    else:
+        print("{} arguments:".format(length - 1))
+        num = 1
+        for arg in argv:
+            if arg == argv[0]:
+                continue
+            print("{}: {}".format((num), arg))
+            num = num + 1
+
+
--- 0x02-python-import_modules/README.md
+++ 0x02-python-import_modules/README.md
@@ -1,3 +1,5 @@
 0x02. Python - import & modules readme file
 
 0. Import a simple function from a simple file
+1. Write a program that imports functions from the file calculator_1.py, does some Maths, and prints the result.
+2. How to make a script dynamic! Write a program that prints the number of and the list of its arguments.
