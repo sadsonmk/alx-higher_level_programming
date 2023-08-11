653b57856b3dcc8626c128a4cee11c20cc1c609e
Sadson Mk <sadsonmkumira@yahoo.com>
2023-08-11 09:45:59 -0700

    Adding files

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 
-from magic_calculation_102 import add, sub
 def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
-        for x in range(4, 7):
-            c += x
+        for x in range(4, 6):
+            c = add(c, x)
         return c
     else:
         return sub(a, b)
-
-if __name__ == "__main__":
-    magic_calculation(a, b)
