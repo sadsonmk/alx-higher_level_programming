976d6810773dd25d30ee24cfbb7bf57cf4b57822
Sadson Mk <sadsonmkumira@yahoo.com>
2023-08-11 03:27:19 -0700

    Adding files

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,6 +2,7 @@
 
 from calculator_1 import add, sub, mul, div
 from sys import argv, exit
+
 if __name__ == "__main__":
     args = len(argv) - 1
     if args != 3:
@@ -9,6 +10,7 @@ if __name__ == "__main__":
         exit(1)
     else:
         arg1 = int(argv[1])
+        arg2 = argv[2]
         arg3 = int(argv[3])
         if argv[2] == '+':
             print("{} + {} = {}".format(arg1, arg3, add(arg1, arg3)))
@@ -18,6 +20,6 @@ if __name__ == "__main__":
             print("{} * {} = {}".format(arg1, arg3, mul(arg1, arg3)))
         elif argv[2] == 'div':
             print("{} / {} = {}".format(arg1, arg3, div(arg1, arg3)))
-        elif argv[2] != '+' or argv[2] != '-' or argv[2] != '*' or argv[2] != '/':
+        elif arg2 != '+' or arg2 != '-' or arg2 != '*' or arg2 != '/':
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
