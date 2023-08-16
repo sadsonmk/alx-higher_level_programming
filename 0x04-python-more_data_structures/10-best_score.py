8baff2e6030b598ea3db60684902e3d8c35e58bb
Sadson Mk <sadsonmkumira@yahoo.com>
2023-08-16 08:25:02 -0700

    Adding files

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,6 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    key = ''
     largest = 0
     for keys, values in a_dictionary.items():
         if a_dictionary[keys] > largest:
