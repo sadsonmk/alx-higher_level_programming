1fafa58d20322067b698d14efc7fca8b7a66e69a
Sadson Mk <sadsonmkumira@yahoo.com>
2023-08-16 08:22:29 -0700

    Adding files

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    key = ''
+    largest = 0
+    for keys, values in a_dictionary.items():
+        if a_dictionary[keys] > largest:
+            largest = a_dictionary[keys]
+            key = keys
+    return key
