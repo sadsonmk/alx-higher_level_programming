104c178a2b01bc1269f20cd5c1dbec3a02b422af
Sadson Mk <sadsonmkumira@yahoo.com>
2023-08-16 09:45:20 -0700

    Adding files

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,11 +4,11 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or not roman_string:
         return 0
 
-    numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    numerls = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     total = 0
     old_value = 0
     for numeral in roman_string:
-        new_value = numerals[numeral]
+        new_value = numerls[numeral]
         if new_value > old_value:
             total -= (2 * old_value)
         total += new_value
