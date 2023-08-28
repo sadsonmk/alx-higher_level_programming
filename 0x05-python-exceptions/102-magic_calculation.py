af9e549b7a2403f4e33a91e4d2af7ad768c49e0a
Sadson Mk <sadsonmkumira@yahoo.com>
2023-08-28 12:38:55 -0700

    Adding files

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -3,8 +3,7 @@ def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
         if i > a:
-            raise Exception("Too far")
-        else:
-            result += (a ** b) / i
+            raise Exception('Too far')
+        result += (a ** b) / i
     result += a + b
     return result
