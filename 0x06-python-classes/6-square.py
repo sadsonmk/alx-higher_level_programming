6706fe307a63e149fa1b53939657715531845f35
Sadson Mk <sadsonmkumira@yahoo.com>
2023-08-29 10:39:38 -0700

    Adding files

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -32,7 +32,7 @@ class Square:
     @position.setter
     def position(self, value):
         """sets the position of the square"""
-        if value[0] < 0 or value[1] < 0:
+        if value[0] < 0 or value[1] < 0 or len(value) != 2:
             raise TypeError('position must be a tuple of 2 positive integers')
         else:
             self.__position = value
