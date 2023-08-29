a516f037f4230c7f21608e1cb1eb9560cbb4b250
Sadson Mk <sadsonmkumira@yahoo.com>
2023-08-29 02:07:35 -0700

    Adding files

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+class Square:
+    '''Defines a square'''
+    def __init__(self, size=0):
+        '''this is a constructor function'''
+        self.__size = size
+
+    @property
+    def size(self):
+        '''returns the size of the square'''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        '''sets the value of size'''
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
+
+    def area(self):
+        '''calculates the area of the square'''
+        return self.__size * self.__size
+
+    def my_print(self):
+        '''prints in stdout the square with the character #'''
+        if self.__size == 0:
+            print()
+        else:
+            for x in range(self.__size):
+                for y in range(self.__size):
+                    print('#', end='')
+                print()
