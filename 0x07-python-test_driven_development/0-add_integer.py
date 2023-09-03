ecfd57d83268362b6b33a1df515d2a4e50b88835
Sadson Mk <sadsonmkumira@yahoo.com>
2023-09-03 12:10:52 -0700

    Adding files

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,10 +3,20 @@
 
 
 def add_integer(a, b=98):
-    """A function that adds two integers"""
+    """A function that adds two integers.
+
+    Args:
+        a (int/float): the first argument to be added.
+        b (int/float): the second argument to be added.
+    Returns:
+        int: The sum of a and b.
+    Raises:
+        TypeError: Raises an exception if a or b is neither an int nor a float.
+    """
+
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError('a must be an integer')
     elif not isinstance(b, int) and not isinstance(b, float):
         raise TypeError('b must be an integer')
     else:
-        return int(a) + int(b)
+        return int(a + b)
