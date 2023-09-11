3f63d0c762992b82625fb0a33f7cd00fb453c5d7
Sadson Mk <sadsonmkumira@yahoo.com>
2023-09-11 01:00:07 -0700

    Adding file 0-lookup.py and readme.md

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""module to return the attributes and methods of an object"""
+
+
+def lookup(obj):
+    """a function to return attributes and methods of an object"""
+
+    return (dir(obj))
