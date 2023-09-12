ffeca276e83edea008a49acf4eff11edf45c1467
Sadson Mk <sadsonmkumira@yahoo.com>
2023-09-12 11:25:15 -0700

    Adding files

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,21 +6,22 @@ import json
 import sys
 
 
-def save_to_json_file(my_obj, filename="add_item.json"):
+def save_to_json_file(my_obj=[], filename="add_item.json"):
     """a function that writes an Object to a text file using JSON"""
 
-    arguments = []
     for args in sys.argv:
         if args is not sys.argv[0]:
-            arguments.append(args)
-    my_obj = arguments
+            my_obj.append(args)
     with open(filename, 'w') as f:
         json.dump(my_obj, f)
 
+    return my_obj
 
-def load_from_json_file(filename="add_item.json"):
+
+def load_from_json_file(filename):
     """a function that creates an Object from a “JSON file"""
 
+    filename = save_to_json_file()
     with open(filename, 'r') as f:
         data = json.load(f)
         return (data)
