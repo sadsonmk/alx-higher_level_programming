d5a334a8d470a44f9de28e2b8a6ac56ca9b39999
Sadson Mk <sadsonmkumira@yahoo.com>
2023-09-12 09:23:45 -0700

    Adding files

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-a module that writes an Object to a 
+a module that writes an Object to a
 text file, using a JSON representation
 """
 import json
@@ -8,9 +8,8 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """a function that writes an Object to a text file using JSON"""
-    
+
     obj = json.dumps(my_obj)
     with open(filename, 'w') as f:
         file_r = f.write(obj)
-    f.close()
-    return file_r        
+    return file_r
