6bb71eec1b44eb386910a003ba228bc81618ccd1
Sadson Mk <sadsonmkumira@yahoo.com>
2023-09-12 09:19:37 -0700

    Adding files

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -5,7 +5,7 @@ import json
 
 def from_json_string(my_str):
     """
-    a function that returns an object 
+    a function that returns an object
     (Python data structure) represented by a JSON string
     """
     output = json.loads(my_str)
