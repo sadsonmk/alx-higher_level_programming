da6b8bc649ec52fa14643019492359862c556dc7
Sadson Mk <sadsonmkumira@yahoo.com>
2023-09-12 08:49:54 -0700

    Adding files

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,6 @@
 def read_file(filename=""):
     """a function to read a file and prints it to stdout"""
 
-    with open(filename, 'r') as f:
+    with open(filename, 'r', encoding='utf8') as f:
         r = f.read()
         print(r)
