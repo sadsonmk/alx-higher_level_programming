906afc3c3cd52569e06c87032a85a3a81fa2e683
Sadson Mk <sadsonmkumira@yahoo.com>
2023-09-13 08:55:51 -0700

    Adding files

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""module that defines a student class"""
+
+
+class Student:
+    """defining a student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """initializing instance attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """converting an object to JSON
+        If attrs is a list of strings, only attribute
+        names contained in this list must be retrieved.
+        """
+        if (type(attrs) == list and all(type(x) == str for x in attrs)):
+            return {key: getattr(self, key)
+                    for key in attrs if hasattr(self, key)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance"""
+        self.first_name = json['first_name']
+        self.last_name = json['last_name']
+        self.age = json['age']
