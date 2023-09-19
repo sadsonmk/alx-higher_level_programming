bc5db318ac802003cceeedd34f22e0a0e85289d9
Sadson Mk <sadsonmkumira@yahoo.com>
2023-09-18 19:27:12 -0700

    Adding files

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -27,13 +27,14 @@ class Base:
     def save_to_file(cls, list_objs):
         """writes the JSON string representation of list_objs to a file"""
 
+        my_file = cls.__name__
         if list_objs is None:
             res = "[]"
-        my_file = cls.__name__
-        res = []
+        else:
+            res = []
 
-        for i in list_objs:
-            res.append(i.to_dictionary())
-        res = cls.to_json_string(my_list)
+            for i in list_objs:
+                res.append(i.to_dictionary())
+            res = cls.to_json_string(my_list)
         with open(f"{my_file}.json", 'w') as f:
             f.write(res)
