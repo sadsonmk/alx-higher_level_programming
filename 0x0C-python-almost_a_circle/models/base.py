e33a963e54120dec95b7568d818a5ac9ce202565
Sadson Mk <sadsonmkumira@yahoo.com>
2023-09-18 19:48:42 -0700

    Adding files

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -43,5 +43,5 @@ class Base:
     def from_json_string(json_string):
         """returns the list of the JSON string representation json_string"""
         if json_string is None:
-            return "[]"
+            return []
         return json.loads(json_string)
