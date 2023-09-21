e88edfdf2bed40838a665f871831ac1260b31d30
Sadson Mk <sadsonmkumira@yahoo.com>
2023-09-21 09:46:12 -0700

    Adding files

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -4,14 +4,40 @@ import unittest
 from models.base import Base
 
 
-class TestBase(unittest.TestCase):
-    """Test class inheriting from unittest"""
-    def tearDown(self):
-        Base._Base__nb_objects = 0
+class TestBase_obj(unittest.TestCase):
+    """Test class inheriting from unittest for Base class objects"""
 
-    def test_base(self):
-        """testing the ids of class base instances"""
-        self.b1 = Base()
+    def test_base_zero_args(self):
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b1.id, b2.id - 1)
 
-        self.assertEqual(self.b1.id, 1)
 
+    def test_numerous_zero_args(self):
+        b1 = Base()
+        b2 = Base()
+        b3 = Base()
+        b4 = Base()
+        self.assertEqual(b1.id, b4.id - 4)
+
+
+    def test_supplied_id(self):
+        self.assertEqual(Base(15).id, 15)
+
+    def test_id_None(self):
+        b1 = Base(None)
+        b2 = Base(None)
+        self.assertEqual(b1.id, b2.id - 1)
+
+    def test_id_string(self):
+        self.assertEqual(Base('hi').id, 'hi')
+
+    def test_id_bool(self):
+        self.assertEqual(Base(True).id, True)
+
+    def test_id_list(self):
+        self.assertEqual(Base([1,2,3,4]).id, [1,2,3,4])
+    
+    def test_lotof_args(self):
+        with self.assertRaises(TypeError):
+            Base(12, 45)
