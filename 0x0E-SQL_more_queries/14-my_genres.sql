ad7d43016ae22602dd62d945fe6286b61d652dd5
Sadson Mk <sadsonmkumira@yahoo.com>
2023-10-18 11:44:44 -0700

    Adding files

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,5 +2,6 @@
 SELECT tv_genres.name
 FROM tv_genres
 JOIN tv_show_genres ON tv_show_genres.genre_id=tv_genres.id
-WHERE tv_show_genres.show_id=8
+JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
+WHERE tv_shows.title = "Dexter"
 ORDER BY tv_genres.name;
