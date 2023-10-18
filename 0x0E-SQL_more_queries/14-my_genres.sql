5cac90ed4153a2dd2f232c32a84bc62502812157
Sadson Mk <sadsonmkumira@yahoo.com>
2023-10-18 11:37:53 -0700

    Adding files

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,6 @@
 -- lists all genres of the show Dexter
 SELECT tv_genres.name
 FROM tv_genres
-LEFT JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
-WHERE tv_show_genres.show_id = 8
+JOIN tv_show_genres ON tv_show_genres.genre_id=tv_genres.id
+WHERE tv_show_genres.show_id=8
 ORDER BY tv_genres.name;
