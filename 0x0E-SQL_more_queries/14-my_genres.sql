3569dc775459fb58e49d12178039a0eed44505a1
Sadson Mk <sadsonmkumira@yahoo.com>
2023-10-18 11:07:25 -0700

    Adding files

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,5 +2,5 @@
 SELECT tv_genres.name
 FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
-WHERE tv_show_genres.show_id=8
+WHERE tv_show_genres.show_id = 8
 ORDER BY tv_genres.name;
