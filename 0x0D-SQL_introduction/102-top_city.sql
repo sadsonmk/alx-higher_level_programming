beb79c200de1ac83ef983158e62b8d5a9cae58af
Sadson Mk <sadsonmkumira@yahoo.com>
2023-10-19 03:12:04 -0700

    Adding files

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+-- displays the top 3 of cities temperature during July and August ordered by temperature (descending)
+SELECT temperatures.city, AVG(temperatures.value) AS 'avg_temp'
+FROM temperatures
+WHERE temperatures.month=7 OR temperatures.month=8
+GROUP BY temperatures.city
+ORDER BY avg_temp DESC
+LIMIT 3;
