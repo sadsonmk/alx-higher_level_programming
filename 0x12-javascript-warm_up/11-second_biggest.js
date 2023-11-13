2ca2c71cd26121c5244788c1bc1fa0aba3fcda41
Sadson <sadsonmkumira@yahoo.com>
2023-11-13 08:23:48 -0800

    Adding the file 11-second_largest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,11 +4,14 @@ const args = process.argv;
 args.shift();
 args.shift();
 
-args.sort();
-
 const len = args.length;
 if (len === 0 || len === 1) {
   console.log(0);
 } else {
-  console.log(args[len - 2]);
+  const myarr = [];
+  for (let a = 0; a < len; a++) {
+    myarr.push(parseInt(args[a]));
+  }
+  myarr.sort();
+  console.log(myarr[len - 2]);
 }
