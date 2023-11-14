07412a11fc96dbdeb06d1d9101dd91912604e58f
Sadson <sadsonmkumira@yahoo.com>
2023-11-14 01:05:58 -0800

    Adding files

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+    if (w <= 0 || h <= 0) {
       this.width = undefined;
       this.height = undefined;
     } else {
