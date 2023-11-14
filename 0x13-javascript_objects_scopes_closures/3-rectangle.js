d3c9033074db3fe5e9ec88cfd549c60ef72fca2c
Sadson <sadsonmkumira@yahoo.com>
2023-11-14 01:03:40 -0800

    Adding files

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+      this.width = undefined;
+      this.height = undefined;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let value = '';
+      for (let j = 0; j < this.width; j++) {
+        value += 'X';
+      }
+      console.log(value);
+    }
+  }
+}
+module.exports = Rectangle;
