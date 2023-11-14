8e1adb96ed928e728d84aea4f9eef2555e992fd9
Sadson <sadsonmkumira@yahoo.com>
2023-11-14 10:04:00 -0800

    Adding files

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,10 +2,13 @@
 const Squares = require('./5-square');
 
 class Square extends Squares {
-  charPrint (c = 'X') {
-    for (let i = 0; i < this.size; i++) {
+  charPrint (c) {
+    if (c === undefined) {
+	    c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
       let value = '';
-      for (let j = 0; j < this.size; j++) {
+      for (let j = 0; j < this.width; j++) {
         value += c;
       }
       console.log(value);
