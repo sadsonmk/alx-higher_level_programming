f32f89265f22a4f629869a165b882acddf9a4008
Sadson <sadsonmkumira@yahoo.com>
2023-11-16 10:50:46 -0800

    Adding the file 1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This is a script that filters states
+listting those that start with the letter N"""
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db_connect = MySQLdb.connect(host='localhost', port=3306, user=user,
+                                 passwd=password, db=database)
+    mycursor = db_connect.cursor()
+
+    sql = "SELECT * FROM states WHERE name LIKE 'N%'"
+    mycursor.execute(sql)
+
+    result = mycursor.fetchall()
+
+    for state in result:
+        print(state)
