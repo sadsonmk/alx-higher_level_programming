fc59e1ba98657eec029409b61b09d36d365e370b
Sadson <sadsonmkumira@yahoo.com>
2023-11-17 08:19:28 -0800

    Adding files

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This is a script that prints the first State
+    object from the database hbtn_0e_6_usa"""
+
+import sys
+from model_state import Base, State, Session
+from sqlalchemy import (create_engine)
+
+
+if __name__ == '__main__':
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    port = 3306
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:{}/{}'.format(user,
+                           password, port, database), pool_pre_ping=True)
+    session = Session(bind=engine)
+
+    state = session.query(State).order_by(State.id).first()
+    print('{}: {}'.format(state.id, state.name))
+
+    session.close()
