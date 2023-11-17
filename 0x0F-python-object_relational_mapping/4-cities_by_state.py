95724d8d3923f75b73cc636fa25323155230a2df
Sadson <sadsonmkumira@yahoo.com>
2023-11-17 01:57:11 -0800

    Adding files

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This is a script that lists all cities from the database hbtn_0e_4_usa"""
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db_connect = MySQLdb.connect(host='localhost', port=3306, user=user,
+                                 passwd=password, db=database, charset='utf8')
+    cur = db_connect.cursor()
+    sql = "SELECT * FROM cities ORDER BY id ASC"
+    cur.execute(sql)
+
+    results = cur.fetchall()
+
+    for city in results:
+        print(city)
+
+    cur.close()
+    db_connect.close()
