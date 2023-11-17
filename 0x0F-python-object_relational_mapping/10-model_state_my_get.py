6de9d1a8bf0827fe4e123207888c484cca39943d
Sadson <sadsonmkumira@yahoo.com>
2023-11-17 10:30:52 -0800

    Adding files

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This is a script that prints the State object
+    with the name passed as argument from the database hbtn_0e_6_usa"""
+
+import sys
+from model_state import Base, State, Session
+from sqlalchemy import (create_engine)
+
+if __name__ == '__main__':
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state = sys.argv[4]
+    port = 3306
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:{}/{}'.format(user,
+                           password, port, database), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(bind=engine)
+
+    for state in session.query(State).filter(State.name == state).all():
+
+        if state:
+            print('{}'.format(state.id))
+        else:
+            print('Not found')
+
+    session.close()
