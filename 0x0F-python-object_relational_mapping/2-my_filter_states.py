011792fb040497e684d49593c41050d2040e6b10
Sadson <sadsonmkumira@yahoo.com>
2023-11-16 19:33:09 -0800

    Adding files

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""a script that takes in an argument and displays all
+values in the states table of hbtn_0e_0_usa where name matches the argument"""
+
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state = sys.argv[4]
+
+    db_connect = MySQLdb.connect(host='localhost', port=3306, user=user,
+                                 passwd=password, db=database)
+    mycursor = db_connect.cursor()
+
+    sql = "SELECT * FROM states WHERE name='{}' ORDER BY id".format(state)
+    mycursor.execute(sql)
+
+    result = mycursor.fetchall()
+
+    for city in result:
+        print(city)
