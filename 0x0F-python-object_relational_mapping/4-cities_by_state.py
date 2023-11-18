3f151cae23002d5730eeb2fb223247d522a6f6d0
Sadson <sadsonmkumira@yahoo.com>
2023-11-18 10:18:15 -0800

    Adding files

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -12,7 +12,8 @@ if __name__ == '__main__':
     db_connect = MySQLdb.connect(host='localhost', port=3306, user=user,
                                  passwd=password, db=database, charset='utf8')
     cur = db_connect.cursor()
-    sql = "SELECT * FROM cities ORDER BY id ASC"
+    sql = """SELECT cities.id, cities.name, states.name FROM cities
+            JOIN states ON states.id=cities.state_id"""
     cur.execute(sql)
 
     results = cur.fetchall()
