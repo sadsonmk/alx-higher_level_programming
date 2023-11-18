850f4d6152df5278a64f42d011c890ad5db7d543
Sadson <sadsonmkumira@yahoo.com>
2023-11-18 09:56:40 -0800

    Adding files

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,9 +9,10 @@ if __name__ == '__main__':
     user = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
+    port = 3306
 
-    db_connect = MySQLdb.connect(host='localhost', port=3306, user=user,
-                                 passwd=password, db=database, charset="utf8")
+    db_connect = MySQLdb.connect(host='localhost', user=user,
+                                 passwd=password, db=database, port=port)
     cur = db_connect.cursor()
 
     sql = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
