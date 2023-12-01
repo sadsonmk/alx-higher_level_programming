4498ea2abc90a769d4442d56f46a04e8b36c11e3
Sadson <sadsonmkumira@yahoo.com>
2023-12-01 04:03:07 -0800

    Adding files

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
     with request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
         result = resp.read()
         print("Body response:")
-        print(f"\t- type: {type(result})")
+        print(f"\t- type: {type(result)}")
         print(f"\t- content: {result}")
         print(f"\t- utf8 content: {result.decode('utf-8')}")
