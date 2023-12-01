bdcbff076bd50bd674582dd3033eb899965cf0d1
Sadson <sadsonmkumira@yahoo.com>
2023-12-01 02:54:38 -0800

    Adding file 0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This a module which uses the urllib module to fetch data from the web"""
+
+
+from urllib import request
+if __name__ == '__main__':
+    resp = request.Request('https://alx-intranet.hbtn.io/status')
+    with request.urlopen(resp) as response:
+        result = response.read()
+        print('Body reponse:')
+        print(f'\t- type: {type(result)}')
+        print(f'\t- content: {result}')
+        print(f'\t- utf8 content: {result.decode("utf-8")}')
