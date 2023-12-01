74b5d9d95464d27f1788ab830782952afca5c418
Sadson <sadsonmkumira@yahoo.com>
2023-12-01 06:31:51 -0800

    Adding values

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -10,4 +10,5 @@ import sys
 if __name__ == '__main__':
     url = sys.argv[1]
     response = requests.get(url)
-    print(response.headers.get("X-Request-Id"))
+    result = response.headers.get("X-Request-Id")
+    print(result)
