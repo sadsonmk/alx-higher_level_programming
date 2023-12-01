2fe232d47b4e4b1a1b3f956cba740627279b6861
Sadson <sadsonmkumira@yahoo.com>
2023-12-01 10:08:10 -0800

    Adding files

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""this is a module that takes 2 arguments and returns commits"""
+
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    repo_name = sys.argv[1]
+    owner = sys.argv[2]
+
+    url = f"https://developer.github.com/repos/{owner}/{repo_name}/commits/"
+    response = requests.get(url)
+
+    res = response.json()
+    i = 0
+    while i < 10:
+        print(f"{res[i].get('sha')}: {res[i].get('commit').get('author')
+                                      .get('name')}")
