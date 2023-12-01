3c84abe88d271623ffee38034cce2df864e9e0cb
Sadson <sadsonmkumira@yahoo.com>
2023-12-01 06:18:00 -0800

    Adding files

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -10,5 +10,6 @@ import sys
 if __name__ == '__main__':
     url = sys.argv[1]
     response = requests.get(url)
-    result = response.headers['X-Request-Id']
-    print(result)
+    for result, value in response.headers.items():
+        if value == response.headers['X-Request-Id']:
+            print(value)
