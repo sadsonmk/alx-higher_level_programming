bb84f8aab7e6fa42aa68e363bb5bf47e310f6667
Sadson <sadsonmkumira@yahoo.com>
2024-01-03 04:55:58 -0800

    Adding the file 4-starwars_count.js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+const charId = 18;
+const charUrl = `https://swapi-api.alx-tools.com/api/people/${charId}/`;
+
+request(url, (error, response, body) => {
+  if (!error) {
+    const data = JSON.parse(body);
+    const dataObjs = data.results;
+
+    const len = dataObjs.length;
+    let total = 0;
+    for (let i = 0; i < len; i++) {
+      const lenObj = dataObjs[i].characters.length;
+      for (let j = 0; j < lenObj; j++) {
+        if (dataObjs[i].characters[j] === charUrl) {
+          total += 1;
+        }
+      }
+    }
+    console.log(total);
+  }
+});
